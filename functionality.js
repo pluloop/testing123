68fd4cef2af663d4f96eaf0c849cb79d895a5d08
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-10 08:52:30 -0700

    update to js file

--- functionality.js
+++ functionality.js
@@ -1,2 +1,17 @@
 var x = 15;
 console.log(15);
+let y = 12;
+const z = 3.14;
+
+console.log(x + y + z);
+console.log("testing testing 123 123");
+
+x = 12;
+y = 1222;
+
+console.log(x + y);
+
+x = 4.212313131312
+y = "hello, world"
+console.log(x + y);
+console.log("This prints PI: " + z);
